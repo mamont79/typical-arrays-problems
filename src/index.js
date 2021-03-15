56b1c79e4a4b1a8e6367a2303af800f925263954
Pavel Dolgov <70911716+mamont79@users.noreply.github.com>
2021-03-15 22:29:12 +0300

    add: solution of task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    if (typeof array[0] == "number") {
+        return Math.min.apply(null, array);
+    } else {
+        return 0;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    let i = 0;
+    let sum = 0;
+    while (i < array.length) {
+        sum = sum + array[i];
+        i++;
+    }
+    let result = sum / array.length;
+    return result.toFixed(2);
+
 }
